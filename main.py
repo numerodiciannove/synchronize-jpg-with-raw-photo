6d0cf8a2f5a26f53fd236418f81fdcdc47b503df
Numero Diciannove <117291303+numerodiciannove@users.noreply.github.com>
2024-02-12 21:54:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from glob import glob
 
 JPG_TYPE = ".jpg"
 JPG_DIR = r"C:\Users\numer\Desktop\test\jpg"
@@ -12,33 +13,33 @@ OUTPUT_DIR = r"C:\Users\numer\Desktop\test\sorted"
 
 def synchronize_jpg_with_raw(jpg_directory, raw_directory, output_directory):
     no_raw_version = []
-    jpg_files = [f for f in os.listdir(jpg_directory) if
-                 f.lower().endswith(JPG_TYPE)]
+
+    jpg_files = glob(os.path.join(jpg_directory, f"*{JPG_TYPE}"))
 
     for jpg_file in jpg_files:
-        raw_file = jpg_file[:-len(JPG_TYPE)] + RAW_TYPE
-        if os.path.exists(os.path.join(raw_directory, raw_file)):
+        jpg_filename = os.path.basename(jpg_file)
+        raw_filename = os.path.splitext(jpg_filename)[0] + RAW_TYPE
+        raw_file_path = os.path.join(raw_directory, raw_filename)
+
+        if os.path.exists(raw_file_path):
             print(
-                f"RAW-version for {jpg_file} exists. "
+                f"RAW-version for {jpg_filename} exists. "
                 f"Copying RAW-version to directory."
             )
-            shutil.copy(
-                os.path.join(raw_directory, raw_file), output_directory
-            )
+            shutil.copy(raw_file_path, output_directory)
         else:
-            print(f"No RAW-version found for {jpg_file}.")
-            no_raw_version.append(jpg_file)
+            print(f"No RAW-version found for {jpg_filename}.")
+            no_raw_version.append(jpg_filename)
 
     with open(
             os.path.join(output_directory, "no_raw_version.txt"), "w"
     ) as file:
-        for jpg_file in no_raw_version:
-            file.write(jpg_file + "\n")
+        file.write("\n".join(no_raw_version))
 
     if no_raw_version:
-        print("Some files not copied, check 'no_raw_version.txt'")
+        print("Some files were not copied, check 'no_raw_version.txt'")
     else:
-        print("All files was copied")
+        print("All files were copied")
 
     return no_raw_version
 
