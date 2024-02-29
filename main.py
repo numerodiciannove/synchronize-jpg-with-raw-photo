448011a15ffcbfb55767531f653ae41cc6134db3
numer <5079447@gmail.com>
2024-02-29 22:25:26 +0200

    add gui

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-from glob import glob
 
 JPG_TYPE = ".jpg"
 JPG_DIR = r"C:\Users\numer\Desktop\test\jpg"
@@ -13,35 +12,31 @@ OUTPUT_DIR = r"C:\Users\numer\Desktop\test\sorted"
 
 def synchronize_jpg_with_raw(jpg_directory, raw_directory, output_directory):
     no_raw_version = []
-
-    jpg_files = glob(os.path.join(jpg_directory, f"*{JPG_TYPE}"))
+    jpg_files = [f for f in os.listdir(jpg_directory) if
+                 f.lower().endswith(JPG_TYPE)]
 
     for jpg_file in jpg_files:
-        jpg_filename = os.path.basename(jpg_file)
-        raw_filename = os.path.splitext(jpg_filename)[0] + RAW_TYPE
-        raw_file_path = os.path.join(raw_directory, raw_filename)
-
-        if os.path.exists(raw_file_path):
+        raw_file = jpg_file[:-len(JPG_TYPE)] + RAW_TYPE
+        if os.path.exists(os.path.join(raw_directory, raw_file)):
             print(
-                f"RAW-version for {jpg_filename} exists. "
+                f"RAW-version for {jpg_file} exists. "
                 f"Copying RAW-version to directory."
             )
-            shutil.copy(raw_file_path, output_directory)
+            shutil.copy(
+                os.path.join(raw_directory, raw_file), output_directory
+            )
         else:
-            print(f"No RAW-version found for {jpg_filename}.")
-            no_raw_version.append(jpg_filename)
+            print(f"No RAW-version found for {jpg_file}.")
+            no_raw_version.append(jpg_file)
 
     with open(
             os.path.join(output_directory, "no_raw_version.txt"), "w"
     ) as file:
-        file.write("\n".join(no_raw_version))
-
-    if no_raw_version:
-        print("Some files were not copied, check 'no_raw_version.txt'")
-    else:
-        print("All files were copied")
+        for jpg_file in no_raw_version:
+            file.write(jpg_file + "\n")
 
     return no_raw_version
 
 
 copied_raw_files = synchronize_jpg_with_raw(JPG_DIR, RAW_DIR, OUTPUT_DIR)
+print("Copied RAW files:", copied_raw_files)
